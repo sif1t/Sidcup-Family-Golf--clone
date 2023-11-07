0763fcb4eaa1881ef40e421eacf76578b8f7c73e
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-11-07 21:28:46 +0600

    new section

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 gsap.to("#nav",{
     backgroundColor :"#000",
+    height: "110px",
     duration:1,
-    delay: 1
+    delay: 0.5,
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        // markers: true,
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1
+    }
+
 })
