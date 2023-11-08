42f840d4e31fc25f30bdabbafb7e17a0885e97d8
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-11-08 17:53:03 +0600

    add new 2 about section-pic

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ var blur = document.querySelector("#cursor-blur")
 document.addEventListener("mousemove",function(dets){
       crsr.style.left = dets.x+"px"
       crsr.style.top = dets.y+"px"
-      blur.style.left = dets.x- 200 + "px"
-      blur.style.top = dets.y- 200 + "px"
+      blur.style.left = dets.x- 250 + "px"
+      blur.style.top = dets.y- 250 + "px"
 })
 
 
