8d9cc5927903a1d62d3786ec854d361bf6575232
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-11-09 18:34:13 +0600

    add new 3 section

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var crsr = document.querySelector("#cursor");
 var blur = document.querySelector("#cursor-blur");
 
 document.addEventListener("mousemove", function (dets) {
-  crsr.style.left = dets.x + "px";
+  crsr.style.left = dets.x +30 + "px";
   crsr.style.top = dets.y + "px";
   blur.style.left = dets.x - 250 + "px";
   blur.style.top = dets.y - 250 + "px";
@@ -12,6 +12,7 @@ document.addEventListener("mousemove", function (dets) {
 
 gsap.to("#nav", {
     backgroundColor: "#000",
+    rotation: 360,
     duration: 0.5,
     height: "110px",
     scrollTrigger: {
@@ -35,3 +36,33 @@ gsap.to("#nav", {
       scrub: 2,
     },
   });
+ 
+var h4all =document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+        crsr.style.scale = 3
+        crsr.style.border = "1px solid #fff"
+        crsr.style.backgroundColor = "transparent"
+    })
+    elem.addEventListener("mouseleave", function(){
+        crsr.style.scale = 1
+        crsr.style.border = "0px solid #91B508"
+        crsr.style.backgroundColor = "#91B508"
+    })
+})
+
+gsap.from("#about-us img,#about-us-in",{
+  y:50,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+   trigger:"#about-us",
+   scroller:"body",
+   start:"top 60%",
+   end: "top 55%",
+   scrub:3
+  }
+
+})
+
+  
