4fcdbf35d80f15ad50db15ca0544c8eefd223d8a
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-11-09 15:39:10 +0600

    modifie 3 section

--- main.js
+++ main.js
@@ -1,38 +1,37 @@
 // cursor 
-var crsr = document.querySelector("#cursor")
-var blur = document.querySelector("#cursor-blur")
-document.addEventListener("mousemove",function(dets){
-      crsr.style.left = dets.x+"px"
-      crsr.style.top = dets.y+"px"
-      blur.style.left = dets.x- 250 + "px"
-      blur.style.top = dets.y- 250 + "px"
-})
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
+});
 
 
-gsap.to("#nav",{
-    backgroundColor :"#000",
-    height: "110px",
-    duration:1,
-    delay: 0.5,
-    scrollTrigger: {
-        trigger: "#nav",
-        scroller: "body",
-        // markers: true,
-        start: "top -10%",
-        end: "top -11%",
-        scrub: 1
-    }
-
-})
-
-gsap.to("#main",{
+gsap.to("#nav", {
     backgroundColor: "#000",
-    scrollTrigger:{
-        trigger:"#main",
-        scroller:"body",
-        markers: true,
-        start: "top -25%",
-        end: "top -70%",
-        scrub:2
-    }
-})
+    duration: 0.5,
+    height: "110px",
+    scrollTrigger: {
+      trigger: "#nav",
+      scroller: "body",
+      // markers:true,
+      start: "top -10%",
+      end: "top -11%",
+      scrub: 1,
+    },
+  });
+  
+  gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+      trigger: "#main",
+      scroller: "body",
+      // markers: true,
+      start: "top -25%",
+      end: "top -70%",
+      scrub: 2,
+    },
+  });
