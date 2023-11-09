0dec7ae33b8bdeed3fdce4d57987d841c9b6df99
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-11-09 19:20:05 +0600

    add new section page4

--- main.js
+++ main.js
@@ -52,15 +52,56 @@ h4all.forEach(function(elem){
 })
 
 gsap.from("#about-us img,#about-us-in",{
-  y:50,
+  y:90,
   opacity:0,
   duration:1,
   scrollTrigger:{
    trigger:"#about-us",
    scroller:"body",
    start:"top 60%",
-   end: "top 55%",
-   scrub:3
+   end:"top 55%",
+   scrub:1
+  }
+
+})
+gsap.from(".card",{
+  scale:0.8,
+  opacity:0,
+  duration:1,
+  stagger:0.1,
+  scrollTrigger:{
+   trigger:".card",
+   scroller:"body",
+   markes: true,
+   start:"top 70%",
+   end:"top 65%",
+   scrub:1
+  }
+
+})
+gsap.from("#colon1",{
+   y: -70,
+   x: -70,
+  scrollTrigger:{
+    trigger: "#colon1",
+    scroller: "body",
+    markes: true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub:4
+  }
+
+})
+gsap.from("#colon2",{
+   y: 70,
+   x: 70,
+  scrollTrigger:{
+    trigger: "#colon1",
+    scroller: "body",
+    markes: true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub:4
   }
 
 })
